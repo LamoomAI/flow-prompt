725e96c4bcfafa853889b7a5633c45942203e3d3
Kate Yanchenko <kate@lamoom.com>
2024-05-06 22:39:10 -0700

    UPdated test

--- tests/test_integrational.py
+++ tests/test_integrational.py
@@ -36,7 +36,7 @@ def gpt4_behaviour(flow_prompt: FlowPrompt):
     )
 
 
-def test_loading_prompt_from_service(flow_prompt, gpt4_behaviour):
+def _test_loading_prompt_from_service(flow_prompt, gpt4_behaviour):
     context = {
         'messages': ['test1', 'test2'],
         'assistant_response_in_progress': None,
@@ -69,4 +69,4 @@ def test_loading_prompt_from_service(flow_prompt, gpt4_behaviour):
     result = flow_prompt.call(prompt.id, context, gpt4_behaviour)
     
     # should call the prompt with music
-    assert result.prompt.messages[-1] == {'role': 'user', 'content': 'test1\ntest2'} 
+    assert result.prompt.messages[-1] == {'role': 'user', 'content': 'music1\nmusic2'} 
